3e7e2735f05e0209ec1f06c2775da4763ea6b572
Annie Araújo <annie.a.alves@gmail.com>
2023-12-19 02:22:15 -0300

    Resolução do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,26 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone testNokia = new Nokia("11980001234", "Nokia X", "A12B345", 128);
+Smartphone testIPhone = new Iphone("11980102030", "Iphone ZFlip", "Z01Z000", 64);
+
+//O output deve ser "Ligando..."
+testNokia.Ligar();
+testIPhone.Ligar();
+
+Console.WriteLine("\n");
+
+//O output deve ser "Recebendo ligação..."
+testNokia.ReceberLigacao();
+testIPhone.ReceberLigacao();
+
+Console.WriteLine("\n");
+
+//Teste de aplicação de instalar um app
+
+testNokia.InstalarAplicativo("Telegram");
+testIPhone.InstalarAplicativo("Telegram");
+
+
+
